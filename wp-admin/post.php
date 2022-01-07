aea08c11a220e4d7787cc3451228bb2014d021a0
sharabheshwara <79836186+sharabheshwara@users.noreply.github.com>
2022-01-07 11:33:43 +0530

    first preview
    first preview

---
+++ wp-admin/post.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=https://dev-ntruth.pantheonsite.io/wp-login.php?redirect_to=https%3A%2F%2Fdev-ntruth.pantheonsite.io%2Fwp-admin%2Fpost.php&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://dev-ntruth.pantheonsite.io/wp-login.php?redirect_to=https%3A%2F%2Fdev-ntruth.pantheonsite.io%2Fwp-admin%2Fpost.php&reauth=1";
+		</script>
+
+		<p>You are being redirected to <a href="https://dev-ntruth.pantheonsite.io/wp-login.php?redirect_to=https%3A%2F%2Fdev-ntruth.pantheonsite.io%2Fwp-admin%2Fpost.php&reauth=1">https://dev-ntruth.pantheonsite.io/wp-login.php?redirect_to=https%3A%2F%2Fdev-ntruth.pantheonsite.io%2Fwp-admin%2Fpost.php&reauth=1</a></p>
+	</body>
+</html>
